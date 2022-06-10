4c8856ac5f289cc0b0e83fbd77b2d9e25fee195a
revobrera <rev.obrera@gmail.com>
2022-06-10 06:08:04 -0400

    corrected removed code from merge

--- StellarMap/env.py
+++ StellarMap/env.py
@@ -9,7 +9,7 @@ class envHelpers:
     def set_testnet_network(self):
         os.environ['DEBUG'] = 'True'
         os.environ['NETWORK'] = 'testnet'
-        os.environ['BASE_HORIZON'] = 'horizon-testnet.stellar.org'
+        os.environ['BASE_HORIZON'] = 'https://horizon-testnet.stellar.org'
         self.set_base_network()
 
     def set_public_network(self):
@@ -19,7 +19,12 @@ class envHelpers:
         self.set_base_network()
 
     def set_base_network(self):
-        # stellar.expert
+        # stellar.expert site
+        os.environ['BASE_SITE'] = 'https://stellar.expert'
+        os.environ['BASE_SITE_NETWORK'] = os.getenv('BASE_SITE') + '/explorer/' + os.getenv('NETWORK')
+        os.environ['BASE_SITE_NETWORK_ACCOUNT'] = os.getenv('BASE_SITE_NETWORK') + '/account/'
+
+        # stellar.expert api
         os.environ['BASE_SE'] = 'https://api.stellar.expert'
         os.environ['BASE_SE_NETWORK'] = os.getenv('BASE_SE') + '/explorer/' + os.getenv('NETWORK')
         os.environ['BASE_SE_NETWORK_ACCOUNT'] = os.getenv('BASE_SE_NETWORK') + '/account/'
