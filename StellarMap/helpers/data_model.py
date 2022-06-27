e655292153d681422eb4c0ae71ef656c002aca21
revobrera <rev.obrera@gmail.com>
2022-06-27 00:35:05 -0400

    sorted imports for data_model

--- StellarMap/helpers/data_model.py
+++ StellarMap/helpers/data_model.py
@@ -1,5 +1,4 @@
-from PyQt5.QtCore import (  # PyQt5 libraries and sub-libaries
-    QAbstractTableModel, Qt)
+from PyQt5.QtCore import QAbstractTableModel, Qt
 
 
 class PandasModel(QAbstractTableModel):
